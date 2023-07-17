f487b4173e0118ca26de021f38d2bd9d06a4430c
DeenoDev <deyanoklaasen@outlook.com>
2023-07-17 22:14:05 +0200

    created play function that is connected to a click event listener.

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ const scoreboard = {
     player: 0,
     computer: 0
 }
+
+//Play game
+function play(e){
+    restart.style.display = 'inline-block';
+    const playerChoice = (e.target.id);
+
+}
+
+//Event listeners
+choices.forEach(choice => choice.addEventListener('click', play));
+
