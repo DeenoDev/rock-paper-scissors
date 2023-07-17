de2dcb74a043d6a1ebc51ff89f6d1b8fa12bf9a0
DeenoDev <deyanoklaasen@outlook.com>
2023-07-17 21:49:20 +0200

    Started with main.js for game logic. Selected all relative elements as JS variables.

---
+++ main.js
@@ -0,0 +1,9 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('score');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
