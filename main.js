02c34a77198e90abc0370897a3bd359b2c4a71e4
DeenoDev <deyanoklaasen@outlook.com>
2023-07-18 17:38:13 +0200

    created get computerchoice function with the random functionality.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const choices = document.querySelectorAll('.choice');
 const score = document.getElementById('score');
 const result = document.getElementById('result');
-const restart = document.getElementById('score');
+const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
 const scoreboard = {
     player: 0,
@@ -12,7 +12,21 @@ const scoreboard = {
 function play(e){
     restart.style.display = 'inline-block';
     const playerChoice = (e.target.id);
+    const computerChoice = getComputerChoice();
 
+}
+
+//Get computer choice
+function getComputerChoice(){
+    const rand = Math.random();
+    if (rand < 0.34) {
+        return 'rock';
+    } else if(rand <= 0.67) {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
 }
 
 //Event listeners
